a778b955c5587cad7d2a407e744fb250bbe810cc
andrewhall <hallal17@wfu.edu>
2019-09-02 14:48:54 -0400

    Adding comment

--- main.cpp
+++ main.cpp
@@ -1,26 +1,41 @@
 #include <iostream>
 Using namespace std;
+#include <cmath>;
+#include <string>;
 
 
 int main() {
 
     int num1;
     int num2;
+
+    int num 3;
+    int num 4;
+
     int sum1;
     int sum 2;
-    char 'x';
-    char 'y';
 
+    int x;
+    int y;
 
     cin >> num1;
     cin >> num2;
+    cin >> sum 1;
+
+    cin >> num3;
+    cin >> num4;
+    cin >> sum2;
 
 
-    sum = num1 * x + num2 * y;
-
-
-
-
+    for( x = -10, x < 11; x++){
+        for( y = -10; y < 11; y++){
+            if(((num1 * x) + (num2 * y) == sum1) && ((num3 * x) + (num4 * y) == sum2)){
+                cout << x << endl;
+                cout << y << endl;
+            }
+        }
+    }
+cout << "No solution";
 
     return 0;
 }
